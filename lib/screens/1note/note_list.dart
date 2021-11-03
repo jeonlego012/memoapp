dddb6db22ac5a7dea1f30e5c9b3e97ca1ef22e2a
jeonlego012 <jyo9873@gmail.com>
2021-11-03 22:09:32 +0900

    note search

--- lib/screens/1note/note_list.dart
+++ lib/screens/1note/note_list.dart
@@ -18,9 +18,20 @@ class NoteList extends StatefulWidget {
 class _NoteListState extends State<NoteList> {
   StreamSubscription<QuerySnapshot>? _currentSubscription;
   List<Note> _notes = <Note>[];
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
 
   _NoteListState() {
     _currentSubscription = loadAllNotes().listen(_updateNotes);
+    _searchController.addListener(() {
+      setState(() {
+        if (_searchController.text.isEmpty) {
+          _searchText = "";
+        } else {
+          _searchText = _searchController.text;
+        }
+      });
+    });
   }
 
   @override
@@ -39,9 +50,10 @@ class _NoteListState extends State<NoteList> {
   Widget build(BuildContext context) {
     Widget noteBody = Column(
       children: [
+        buildSearchBox(),
         Expanded(
           child: ListView(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
             children: _buildNoteList(),
           ),
         ),
@@ -56,14 +68,49 @@ class _NoteListState extends State<NoteList> {
             MaterialPageRoute(builder: (context) => NoteAddScreen()),
           );
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         backgroundColor: Colors.orange,
       ),
     );
   }
 
+  Container buildSearchBox() {
+    return Container(
+      height: 40.0,
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        color: Colors.grey.withOpacity(0.4),
+      ),
+      child: TextField(
+        controller: _searchController,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: Icon(Icons.search),
+          hintText: "검색",
+        ),
+      ),
+    );
+  }
+
   List<ChildNote> _buildNoteList() {
-    return _notes.map((contact) => ChildNote(contact)).toList();
+    if (_searchText.isEmpty) {
+      return _notes.map((contact) => ChildNote(contact)).toList();
+    } else {
+      List<Note> _searchNote = [];
+      String _title, _content;
+      for (int i = 0; i < _notes.length; i++) {
+        _title = _notes.elementAt(i).title;
+        _content = _notes.elementAt(i).content;
+        if (_title.toLowerCase().contains(_searchText.toLowerCase()) ||
+            _content.toLowerCase().contains(_searchText.toLowerCase())) {
+          _searchNote.add(_notes.elementAt(i));
+        }
+      }
+      return _searchNote.map((contact) => ChildNote(contact)).toList();
+    }
   }
 }
 
@@ -85,31 +132,34 @@ class ChildNote extends StatelessWidget {
       child: Dismissible(
         background: Container(
           alignment: Alignment.centerLeft,
-          padding: EdgeInsets.only(left: 20.0),
+          padding: const EdgeInsets.only(left: 20.0),
           color: Colors.red,
-          child: Icon(Icons.delete),
+          child: const Icon(Icons.delete),
         ),
         secondaryBackground: Container(
           alignment: Alignment.centerRight,
-          padding: EdgeInsets.only(right: 20.0),
+          padding: const EdgeInsets.only(right: 20.0),
           color: Colors.red,
-          child: Icon(Icons.delete),
+          child: const Icon(Icons.delete),
         ),
         key: Key(note.title),
         onDismissed: (direction) {
           deleteNote(note.id!);
           ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text('deleted!')));
+              .showSnackBar(const SnackBar(content: Text('삭제됨!')));
         },
         child: ListTile(
-          title: Text(note.title),
+          title: Text(
+            note.title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
           subtitle: Text(
             note.content,
             maxLines: 3,
           ),
           trailing: Text(
             DateFormat('yyyy. MM. dd').format(note.creationTime.toDate()),
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 11.0,
             ),
           ),
